7d670bf837fb96473c15cbda1fb38dfb5a425bd0
MSeal <mseal007@gmail.com>
2015-11-08 16:01:36 -0800

    Added version bump for pypi

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ def readMD(fname):
     else:
         return read(fname)
 
-version = '0.0.1'
+version = '0.0.2'
 required = [req.strip() for req in read('requirements.txt').splitlines() if req.strip()]
 setup(
     name='og-utils',
